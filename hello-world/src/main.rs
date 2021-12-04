4a2d0457d618f7f1c997ca935aad770256487668
Alex Kazakov <kazakov.a.s.90@gmail.com>
2021-12-04 00:39:15 +0000

    Add simple guesser

---
+++ hello-world/src/main.rs
@@ -0,0 +1,27 @@
+use std::cmp::Ordering;
+use std::io;
+use rand::Rng;
+
+fn main() {
+    let secret = rand::thread_rng().gen_range(1..101);
+    // println!("secret number = {}", secret);
+
+    loop {
+        println!("your guess ->");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("not a number");
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("too small!"),
+            Ordering::Greater => println!("too big!"),
+            Ordering::Equal => {
+                println!("you win!");
+                break;
+            }
+        }
+    }
+}
