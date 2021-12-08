d977076b411cf828edeecc5b970499d17c56bbb2
Alex Kazakov <kazakov.a.s.90@gmail.com>
2021-12-08 23:46:14 +0000

    Add tuples

--- battlefield/src/main.rs
+++ battlefield/src/main.rs
@@ -1,12 +1,13 @@
 fn main() {
-    let x = 6;
-    println!("1: x = {}", x);
+    let tup = (500, 6.4, 1);
 
-    let x = 7;
-    println!("2: x = {}", x);
-    {
-        let x = 8;
-        println!("3: x = {}", x);
-    }
-    println!("4: x = {}", x);
+    println!("tup {:?}", create_tuple());
+    println!("Tup1 {}", tup.0);
+    println!("Tup2 {}", tup.1);
+    println!("Tup3 {}", tup.2);
+    println!("The value of tup is: {:?}", tup);
+}
+
+fn create_tuple() -> (i32, f64, u8) {
+    return (500, 6.4, 1);
 }
