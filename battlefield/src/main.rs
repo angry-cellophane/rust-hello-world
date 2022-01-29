0462333a23237cad7332aea92f22c2a4fe689dce
Alex Kazakov <kazakov.a.s.90@gmail.com>
2022-01-29 23:58:11 +0000

    Methods in rust

--- battlefield/src/main.rs
+++ battlefield/src/main.rs
@@ -1,33 +1,20 @@
-fn main() {
-    let s = String::from("Hello salut hola");
-    let word = find_word(&s, 3);
-
-    println!("{}", word);
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn find_word(s: &str, word_number: usize) -> &str {
-    let bytes = s.as_bytes();
-    let mut wn = 0;
-
-    let mut start = 0;
-    let mut start_defined = false;
-    let mut end = 0;
-
-    while end < bytes.len() {
-        if !start_defined && bytes[end] != b' ' {
-            start = end;
-            start_defined = true;
-        }
-        if bytes[end] == b' ' {
-            if wn < word_number {
-                wn += 1;
-                start_defined = false;
-            } else {
-                break;
-            }
-        }
-        end += 1;
+impl Rectangle {
+    fn area(&self) -> u32 {
+        return self.width * self.height;
     }
+}
 
-    return &s[start..end];
+
+fn main() {
+    let rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!("square = {:?}", rect.area());
 }
