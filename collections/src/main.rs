4060f427c3729e4eaa1cf269f66f83906e00fbc2
Aleksandr Kazakov <alexkazakov@fb.com>
2023-07-30 19:47:12 +0100

    Collections

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -3,12 +3,18 @@ struct Item {
     value: String,
 }
 
+fn do_stuff(item: &Item) {
+    let mut v = vec![item];
+    v.push(&Item {
+        value: String::from("2"),
+    });
+}
+
 fn main() {
-    let mut v = vec![1,2,3];
-    for i in &mut v {
-        *i += 1;
-    }
-    for i in &v {
-        println!("el = {}", i);
-    }
+    let i = Item {
+        value: String::from("1"),
+    };
+    do_stuff(&i);
+    println!("{:?}", &i);
+
 }
