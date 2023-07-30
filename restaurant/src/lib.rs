9a309dcd32044478b13fb044c0876797c97f6185
Aleksandr Kazakov <alexkazakov@fb.com>
2023-07-30 19:12:45 +0100

    MOdules

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,19 +1,4 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
-
+mod front_of_house;
 mod back_of_house {
     #[derive(Debug)]
     pub struct Breakfast {
@@ -31,8 +16,8 @@ mod back_of_house {
     }
 }
 
+use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-    meal.toast = String::from("wheat");
-    println!("toast {}", meal.toast);
+    hosting::add_to_waitlist();
 }
