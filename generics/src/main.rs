ab0f33f59033c829241c139d1af859b8f58a501f
Aleksandr Kazakov <alexkazakov@fb.com>
2023-08-28 21:56:04 +0100

    Lifetime

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,3 +1,11 @@
+fn longest<'a>(s1: &'a str, s2: &'a str) -> &'a str {
+    if s1.len() >= s2.len() {
+        s1
+    } else {
+        s2
+    }
+}
+
 fn max<T: std::cmp::PartialOrd>(list: &[T]) -> Option<&T> {
     if list.len() == 0 {
         return None;
@@ -17,4 +25,12 @@ fn main() {
     let v = vec![1,2,3,4,5];
     let max = max(&v);
     println!("max = {:?}", max);
+
+    let s1 = String::from("hello");
+    let s3: &str;
+    {
+        let s2 = String::from("world!");
+        s3 = longest(s1.as_str(), s2.as_str());
+        println!("longest string = {}", s3);
+    }
 }
