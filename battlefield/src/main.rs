d14d361d2310605e29f0625ccaca682c71178e7e
Aleksandr Kazakov <alexkazakov@fb.com>
2024-05-24 11:43:00 +0100

    check walkdir

--- battlefield/src/main.rs
+++ battlefield/src/main.rs
@@ -1,35 +1,7 @@
-use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
+use walkdir::WalkDir;
 
 fn main() {
-    println!("Guessing game");
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    loop {
-        println!("Input you guess");
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess)
-            .expect("Failed");
-
-        if guess.trim().eq("quit") {
-            println!("Bye!");
-            break;
-        }
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        println!("You guessed {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small"),
-            Ordering::Greater => println!("Too big"),
-            Ordering::Equal => {
-                println!("You win");
-                break;
-            },
-        }
-    }
+	for entry in WalkDir::new("/Users/alexkazakov/src/github/angry-cellophane/rust-hello-world") {
+		print!("path = {:?}", &entry);
+	}
 }
